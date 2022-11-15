164777a266cab353e840ac0df582b45fe39bc448
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2022-11-15 17:48:09 +0100

    Add files via upload

---
+++ Tema2eje30.java
@@ -0,0 +1,14 @@
+
+package tema2eje30;
+
+
+public class Tema2eje30 {
+
+  
+    public static void main(String[] args) {
+    /*A) Es una checkedException ya que el problema es de java y el ordenador no de el programador*/
+    /*B) Es una RunTimeException ya que el problema es porque ha introducido mal el tipo de datos como numeros negativos*/
+    /*C) Es una RunTimeException ya que has invocado un metodo en un momento inadecuado*/
+    }
+    
+}
