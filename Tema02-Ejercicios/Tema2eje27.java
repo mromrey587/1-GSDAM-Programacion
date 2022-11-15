75412f2a92a2ee03c4938b5cfc1c5393defdb50e
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2022-11-15 17:20:52 +0100

    Update Tema2eje27.java

--- Tema02-Ejercicios/Tema2eje27.java
+++ Tema02-Ejercicios/Tema2eje27.java
@@ -9,9 +9,13 @@ public class Tema2eje27 {
     public static void main(String[] args) {
         System.out.println("Escribe una direccion IP");
         String b=new Scanner(System.in).nextLine();
+        System.out.println("Escribe una tiempo(ms)");
+        int c=new Scanner(System.in).nextInt();
+        
         try{
         InetAddress m=InetAddress.getByName(b);
-            System.out.println(m.isReachable(10));}
+            System.out.println(m.isReachable(c));
+            System.out.println(m.toString());}
         catch(Exception e){
             System.out.println("No se puede en ese tiempo");}
         
