9a269b457b58e5e22cd2d56dd1babb6c6b2e77a6
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2022-11-22 17:47:27 +0100

    Update JuegoPalabras.java

--- Tema03-Ejercicios/JuegoPalabras.java
+++ Tema03-Ejercicios/JuegoPalabras.java
@@ -19,24 +19,48 @@ public class JuegoPalabras {
     final String black = "\u001B[30m";
     
     
-        System.out.println("Empecemos; escribe tu primera palabra(Son 59)"+red+"\n(AVISO: TE PUEDES RENDIR PONIENDO 'no puedo')"+black );
+        System.out.println("Empecemos; primero elige un nivel de dificultad poniendo el numero entero que lleva delante:\n1-facil(15 ACIERTOS)\n2-medio(30 ACIERTOS)\n3-dificil(59 ACIERTOS)\n"+red+"(AVISO SI NO PONES UN NUMERO ENTERO DEL 1 AL 3 SE TE PONDRA EN DIFICL COMO PREDETERMINADO"+black);
     
-    for(int punt=0;punt<(list.length);){
+        
+    String r=new Scanner(System.in).nextLine();
+    try{int a=Integer.parseInt(r);
+    int max;
+    switch(a){
+        case 1:
+            max=15;
+            System.out.println("Nivel Facil");
+            break;
+        case 2:
+            max=30;
+            System.out.println("Nivel Medio");
+            break;
+        case 3:
+            max=59;
+            System.out.println("Nivel Dificil");
+            break;
+        default:
+            max=59;
+            System.out.println("Nivel Dificil");
+            break;
+    }
+    
+    for(int punt=0;punt<max;){
         String pal=new Scanner(System.in).nextLine();   
     for(int x=0;x<(list.length);x++){
     
     if (pal.equals(list[x])){punt++;
         System.out.println(green+list[x]+" si vale"+black);
-        int y=list.length-punt;
+        list[x]="jiapsdj";
+        int y=max-punt;
         System.out.println("Te quedan:"+y);
-            if ((punt>58)&&(punt<80)){System.out.println(green+"ENHORABUENA HAS GANADO");}}
+            if ((punt>(max-1))&&(punt<80)){System.out.println(green+"ENHORABUENA HAS GANADO");}}
             else{if (pal.equals("no puedo")){
             punt=80;
             System.out.println(red+"has fallado");}}
     
     }  
     }
-    
+    }catch(Exception e){System.out.println("No se puede con ese caracter");}
        
     }
 
