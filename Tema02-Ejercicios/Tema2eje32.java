6091427d5f2ed380d1c3c70afd48609fb0b87f87
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2022-11-28 19:58:17 +0100

    Add files via upload

---
+++ Tema02-Ejercicios/Tema2eje32.java
@@ -0,0 +1,23 @@
+
+package tema2eje32;
+
+import java.io.File;
+import bpc.daw.reproductor.*;
+
+public class Tema2eje32 {
+
+
+    public static void main(String[] args) {
+    File dk=new File("C:/Users/manue/Desktop/PL.mp3");
+    String PL=dk.toString();
+    
+    ArchivoMP3 m=new ArchivoMP3(PL);
+    System.out.println(m.getAutor());
+    Reproductor re=new Reproductor(m,true,true);
+    re.play();
+    
+    
+    
+    }
+
+}
