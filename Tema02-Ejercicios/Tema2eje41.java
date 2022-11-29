f717e41f944c89894b549156d16f51f7e1ad2e26
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2022-11-29 17:04:34 +0100

    Add files via upload

---
+++ Tema02-Ejercicios/Tema2eje41.java
@@ -0,0 +1,13 @@
+
+package tema2eje41;
+
+
+public class Tema2eje41 {
+
+
+    public static void main(String[] args) {
+    double sol=Math.pow((0.38*4.93),(-0.36));
+     System.out.println(sol+"\n"+Math.round(sol));
+    }
+
+}
---
+++ Tema02-Ejercicios/Tema2eje42.java
@@ -0,0 +1,13 @@
+
+package tema2eje42;
+
+
+public class Tema2eje42 {
+
+
+    public static void main(String[] args) {
+    double a=65+(24-65)*Math.random();
+        System.out.println(a);
+    }
+
+}
