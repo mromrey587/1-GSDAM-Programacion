8af8b30cf24516ee550abfae09468609af5317a0
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2023-02-13 21:34:32 +0100

    Add files via upload

---
+++ Tema04-Ejercicios/Ejercicio16/Tema4_16.java
@@ -0,0 +1,13 @@
+
+package tema4_16;
+import daw.persona.*;
+public class Tema4_16 {
+
+
+    public static void main(String[] args) {
+    DNIMejorado d=new DNIMejorado(51183587);
+    
+        System.out.println(d.getDni());
+    }
+
+}
