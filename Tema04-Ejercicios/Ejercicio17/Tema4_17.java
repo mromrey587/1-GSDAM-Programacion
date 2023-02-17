6fec517300b7a9e16d02651247580da82dda51b2
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2023-02-17 17:18:25 +0100

    Add files via upload

---
+++ Tema04-Ejercicios/Ejercicio17/Tema4_17.java
@@ -0,0 +1,14 @@
+
+package tema4_17;
+
+import daw.persona.*;
+public class Tema4_17 {
+
+
+    public static void main(String[] args) {
+       Oficina of=new Oficina();
+       
+        System.out.println(of.getSueldoMedio());
+    }
+
+}
